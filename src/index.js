886213044b2891526b48ef28701d8d2e00a02cc9
AmanuelAsfaw <amanuelasfawardi@gmail.com>
2022-11-30 15:37:50 +0300

    A Simple Mutation

--- src/index.js
+++ src/index.js
@@ -1,25 +1,13 @@
 const { ApolloServer } = require('apollo-server');
+const fs = require('fs');
+const path = require('path');
 
-// 1
-const typeDefs = `
-  type Query {
-    info: String!
-    feed: [Link!]!
-  }
-
-  type Link {
-    id: ID!
-    description: String!
-    url: String!
-  }
-`
 let links = [{
   id: 'link-0',
   url: 'www.howtographql.com',
   description: 'Fullstack tutorial for GraphQL'
 }]
 
-// 2
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
@@ -29,12 +17,27 @@ const resolvers = {
     id: (parent) => parent.id,
     description: (parent) => parent.description,
     url: (parent) => parent.url,
+  },
+  Mutation: {
+    createPost: (parent, args) => {
+      let idCount = links.length
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    }
   }
 }
 
 // 3
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: fs.readFileSync(
+    path.join(__dirname, 'schema.graphql'),
+    'utf8'
+  ),
   resolvers,
 })
 
